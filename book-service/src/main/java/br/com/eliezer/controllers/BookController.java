e2be32461c678078f0ecc38ddd98a01bbeaef731
eliezermoraesss <eliezer.moraes@outlook.com>
2022-09-04 08:39:25 -0300

    Implemented Load Balancer, API Gateway, RouteLocator and Filters in API Gateway

--- book-service/src/main/java/br/com/eliezer/controllers/BookController.java
+++ book-service/src/main/java/br/com/eliezer/controllers/BookController.java
@@ -37,7 +37,8 @@ public class BookController {
 		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 		
 		var port = environment.getProperty("local.server.port");
-		book.setEnvironment(port + " FEIGN");
+		book.setEnvironment(
+				"Book port: " + port + "  Cambio Port: " + cambio.getEnvironment() );
 		book.setPrice(cambio.getConvertedValue());	
 		return book;
 	}
--- book-service/src/main/java/br/com/eliezer/proxy/CambioProxy.java
+++ book-service/src/main/java/br/com/eliezer/proxy/CambioProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.eliezer.response.Cambio;
 
-@FeignClient(name = "cambio-service", url = "localhost:8000")
+@FeignClient(name = "cambio-service")
 public interface CambioProxy {
 
 	@GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
